ce39b08863a02aa9984d1a6711f005e208aaa8af
roneyyb <bothra.rajat08@gmail.com>
2021-09-15 12:27:41 +0530

    Minor ui fix

--- src/screens/app/edit/product/Color.tsx
+++ src/screens/app/edit/product/Color.tsx
@@ -111,7 +111,7 @@ const headerTitle: headerTitleI[] = [
     },
 ];
 
-const tableFlex = [1.5, 1.5, 3, 2.5, 2.5];
+const tableFlex = [1.3, 1, 2.5, 2.5, 2.5];
 const columnFlex = [0.5, 1, 3, 2.7, 2.7];
 
 interface Error {
