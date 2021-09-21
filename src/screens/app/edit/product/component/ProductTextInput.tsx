5f1a50e2adb1ea0160e1cda2d609bcbfdd00c6ca
roneyyb <bothra.rajat08@gmail.com>
2021-09-21 16:10:25 +0530

    Minor ui fix

--- src/screens/app/edit/product/component/ProductTextInput.tsx
+++ src/screens/app/edit/product/component/ProductTextInput.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { View, ViewStyle } from 'react-native';
 import { fs12, fs13 } from '../../../../../common';
 import WrappedText from '../../../../component/WrappedText';
 import WrappedTextInput from '../../../../component/WrappedTextInput';
-import { BC, BR, BW, HP, MT, AIC, FS, BGCOLOR } from '../../../../../common/styles';
+import { BC, BR, BW, HP, MT, AIC, FS, BGCOLOR, PT } from '../../../../../common/styles';
 import { borderColor } from '../../../../../common/color';
 import { getHP } from '../../../../../common/dimension';
 import { border, borRad } from './generalConfig';
@@ -15,6 +15,8 @@ export interface ProductTitleProps {
     height?: number;
     multiline?: boolean;
     error?: string;
+    textAlignVertical?: 'center' | 'auto' | 'top' | 'bottom';
+    containerStyle?: ViewStyle | ViewStyle[];
 }
 
 const ProductTextInput: React.FC<ProductTitleProps> = ({
@@ -24,6 +26,8 @@ const ProductTextInput: React.FC<ProductTitleProps> = ({
     onChangeText,
     height,
     multiline,
+    textAlignVertical,
+    containerStyle,
 }) => {
     return (
         <WrappedTextInput
@@ -31,13 +35,16 @@ const ProductTextInput: React.FC<ProductTitleProps> = ({
             multiline={multiline}
             onChangeText={onChangeText}
             placeholder={placeholder}
+            textAlignVertical={textAlignVertical}
             containerStyle={[
                 border,
+
                 MT(0.15),
                 HP(height || 0.4),
                 borRad,
                 AIC('flex-start'),
                 { paddingLeft: getHP(0.1) },
+                containerStyle,
             ]}
             errorText={error}
             textInputStyle={[FS(fs12), HP(height || 0.4)]}
