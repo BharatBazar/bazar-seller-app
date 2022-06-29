b295863414be7c693d0300a64c002e3708d65bc7
roneyyb <bothra.rajat08@gmail.com>
2022-06-29 17:22:29 +0530

    Added drawer component ui

--- src/screens/app/drawer/SideMenu.tsx
+++ src/screens/app/drawer/SideMenu.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { DrawerContentComponentProps } from '@react-navigation/drawer';
-import { FontFamily, fs14, fs18, fs20, NavigationProps } from '../../../common';
+import { FontFamily, fs12, fs14, fs18, fs20, NavigationProps } from '../../../common';
 import WrappedRectangleButton from '../../component/WrappedRectangleButton';
 import { NavigationKey } from '../../../labels';
 import WrappedText from '../../component/WrappedText';
 import { getHP, getWP } from '../../../common/dimension';
-import { AIC, BGCOLOR, BR, FDR, M, MH, provideShadow, P } from '../../../common/styles';
-import { black100, mainColor } from '../../../common/color';
+import { AIC, BGCOLOR, BR, FDR, M, MH, provideShadow, P, AS, FLEX, JCC, BW, BC } from '../../../common/styles';
+import { black100, borderColor, mainColor } from '../../../common/color';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 import { Storage, StorageItemKeys } from '../../../storage';
 import { IshopMember } from '../../../server/apis/shopMember/shopMember.interface';
 import { STATUS_BAR_HEIGHT } from '../../component/StatusBar';
+import { BRA, GENERAL_PADDING, MBA, MHA, MTA } from '@app/common/stylesheet';
 
 export const SideMenu = (props: DrawerContentComponentProps & NavigationProps) => {
     async function logOut() {
@@ -48,7 +49,7 @@ export const SideMenu = (props: DrawerContentComponentProps & NavigationProps) =
         <View style={styles.container}>
             {/* <View style={[{ height: STATUS_BAR_HEIGHT }, BGCOLOR(mainColor)]} /> */}
             <View>
-                <View style={[provideShadow(), BGCOLOR(mainColor), M(0.1), P(0.2), BR(0.1)]}>
+                <View style={[BGCOLOR(mainColor), MTA(), MHA(), P(0.2), BRA(10)]}>
                     <WrappedText
                         text={'Hi! ' + userDetail.name}
                         textColor={'#FFFFFF'}
@@ -58,17 +59,48 @@ export const SideMenu = (props: DrawerContentComponentProps & NavigationProps) =
                     <WrappedText text={userDetail.phoneNumber} textColor={'#FFFFFF'} />
                 </View>
             </View>
-            <WrappedRectangleButton
-                containerStyle={[styles.containerStyle, provideShadow(), BGCOLOR('#FFFFFF'), MH(0.2)]}
-                onPress={() => {
-                    logOut();
-                }}
-            >
-                <View style={[FDR(), AIC()]}>
-                    <MaterialIcon name={'logout'} size={fs18} />
-                    <WrappedText text={'Logout'} textStyle={styles.textStyle} />
-                </View>
-            </WrappedRectangleButton>
+            <View style={[MHA(), BGCOLOR('#FFFFFF'), provideShadow(2), BRA(10), MTA()]}>
+                <WrappedRectangleButton
+                    containerStyle={[styles.containerStyle]}
+                    onPress={() => {
+                        logOut();
+                    }}
+                >
+                    <View style={[FDR(), AIC()]}>
+                        <MaterialIcon name={'person'} size={fs18} />
+                        <WrappedText text={'Member Details'} textStyle={styles.textStyle} />
+                    </View>
+                </WrappedRectangleButton>
+                <View style={[BW(0.5), BC(borderColor), MHA()]} />
+                <WrappedRectangleButton
+                    containerStyle={[styles.containerStyle]}
+                    onPress={() => {
+                        logOut();
+                    }}
+                >
+                    <View style={[FDR(), AIC()]}>
+                        <MaterialIcon name={'edit'} size={fs18} />
+                        <WrappedText text={'Shop Details'} textStyle={styles.textStyle} />
+                    </View>
+                </WrappedRectangleButton>
+                <View style={[BW(0.5), BC(borderColor), MHA()]} />
+                <WrappedRectangleButton
+                    containerStyle={[styles.containerStyle]}
+                    onPress={() => {
+                        logOut();
+                    }}
+                >
+                    <View style={[FDR(), AIC()]}>
+                        <MaterialIcon name={'logout'} size={fs18} />
+                        <WrappedText text={'Logout'} textStyle={styles.textStyle} />
+                    </View>
+                </WrappedRectangleButton>
+            </View>
+            {/* <WrappedText
+                text={'App Version 1.2'}
+                textStyle={styles.textStyle}
+                containerStyle={[FLEX(1), JCC('flex-end'), MBA(), AIC()]}
+            /> */}
         </View>
     );
 };
@@ -89,9 +121,9 @@ const styles = StyleSheet.create({
         borderRadius: getWP(0.2),
     },
     textStyle: {
-        color: black100,
-        fontSize: fs14,
-        fontFamily: FontFamily.Regular,
+        color: 'grey',
+        fontSize: fs12,
+        fontFamily: FontFamily.Medium,
         marginLeft: getWP(0.5),
     },
     iconStyle: {
